02d5f069fd46f87af6afc1a83c26c1e02b174987
Carlos Eduardo <cadudevmaster@gmail.com>
2021-12-26 21:22:31 -0300

    melhorado o teste

--- src/tests/unit/helpers/ImageVision.test.ts
+++ src/tests/unit/helpers/ImageVision.test.ts
@@ -2,10 +2,10 @@ import "reflect-metadata";
 import ImageVision from "../../../helpers/ImageVision"
 
 function createInstace(vision) {
-    return new ImageVision(new vision)
+    return new ImageVision(vision)
 }
 
-describe("Testando se o ImageVison retona o status de violencia corretos", () => {
+describe("Testando se o ImageVison retona o status de violencia correto", () => {
     test("Testando retorno de status", async () => {
         const mockVision = jest.fn().mockImplementation(() => ({
             safeSearchDetection: jest.fn().mockImplementation(() => {
@@ -18,11 +18,14 @@ describe("Testando se o ImageVison retona o status de violencia corretos", () =>
                 ]
             })
         }))
-        const instance = createInstace(mockVision)
+        const mock = new mockVision()
+        const instance = createInstace(mock)
+
         const fakePath = 'caminho/fake/da/silva'
 
         const result = await instance.image_classification_violence(fakePath)
 
+        expect(mock.safeSearchDetection).toHaveBeenCalledWith(fakePath)
         expect(result).toBe('BAIXO')
     })
 })
